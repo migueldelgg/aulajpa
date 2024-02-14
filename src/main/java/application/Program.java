82db13925197b497a66b81e2b476625ba695b3bf
4Miguel2 <delgdevoleper@gmail.com>
2024-02-13 23:53:36 -0300

    recuperando dados com jpa

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -9,18 +9,17 @@ import dominio.Pessoa;
 public class Program {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "Miguel Santos", "miguel@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Hemilly Raiane", "hemilly@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Julio Cesar", "julio@gmail.com");
+        
         
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager(); // conexão com banco de dados, contexto de persistência
         
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+        
+        System.out.println(p);
+        
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
